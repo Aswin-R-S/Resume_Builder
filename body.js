2ea1d6caf379fc096ff177cc478114271e8146fd
Aswin RS <aswinsraj165@gmail.com>
2023-06-03 07:38:42 -0600

    first commit

---
+++ body.js
@@ -0,0 +1,90 @@
+function addNewWEField() {
+  
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+  
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+  
+    weOb.insertBefore(newNode, weAddButtonOb);
+}
+
+function addNewAQField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+  
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+  
+    aqOb.insertBefore(newNode, aqAddButtonOb);
+  }
+  
+  //saving cv
+  function generateCV() {
+  
+    let nameField = document.getElementById("nameField").value;
+  
+    let nameT1 = document.getElementById("nameT1");
+  
+    nameT1.innerHTML = nameField;
+  
+    //name
+    document.getElementById("nameT2").innerHTML = nameField;
+
+    //mail
+    document.getElementById("mailT").innerHTML = document.getElementById(
+        "mailField"
+      ).value;
+  
+    //contact
+    document.getElementById("contactT").innerHTML = document.getElementById(
+      "contactField"
+    ).value;
+  
+    //address
+    document.getElementById("addressT").innerHTML = document.getElementById(
+      "addressField"
+    ).value;
+
+    //skill
+  
+    document.getElementById("skillT").innerHTML = document.getElementById(
+        "skillField"
+     ).value;
+  
+    //we
+  
+    let wes = document.getElementsByClassName("weField");
+  
+    let str = "";
+  
+    for (let e of wes) {
+      str = str + `<li> ${e.value} </li>`;
+    }
+  
+    document.getElementById("weT").innerHTML = str;
+  
+    //aq
+  
+    let aqs = document.getElementsByClassName("eqField");
+  
+    let str1 = "";
+  
+    for (let e of aqs) {
+      str1 += `<li> ${e.value} </li>`;
+    }
+  
+    document.getElementById("aqT").innerHTML = str1;
+
+     
+  
+  }
+  
